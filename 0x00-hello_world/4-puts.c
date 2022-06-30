d913726208c9f86bfb67285080462443e8d366ea
urielaweobe <urielaweobe@gmail.com>
2022-06-30 16:48:54 -0700

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+
+0;276;0c#include <stdio.h>
 /**
  *main - Prints out a string to stdout.
  *Return: 0 if success.
  */
 int main(void)
 {
-puts("\"Programming is the building of multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
