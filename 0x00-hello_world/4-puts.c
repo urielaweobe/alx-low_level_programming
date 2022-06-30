ed1042d60f972e93ce5378791db3986ab2f67123
URIEL <urielaweobe@gmail.com>
2022-06-30 23:59:42 +0100

    c is fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Description: main -Prints out a string to stdout.
+ * return: 0 if success.
+ */
+int main(void)
+{
+    puts("\"Programming os the building of multilingual puzzy") return (0);
+}
