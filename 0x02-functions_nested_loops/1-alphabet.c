d87acbdbeb089cac0f0c5acecb37f8475298b695
URIEL <urielaweobe@gmail.com>
2022-07-05 18:24:00 +0100

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point function
+ * 
+ * Description: Prints the aphabet using a library followed by a new line.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+
+while (i < 26)
+{
+_putchar(alphabet[i]);
+i++;
+}
+_putchar('\n');
+}
