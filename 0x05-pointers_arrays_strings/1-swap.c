95361dc790c3af50331584053af3f754e0785325
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-12 22:48:59 +0100

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int -Function to swap values
+ * 
+ * @a: A value to swap
+ * @b: A value to swap
+ * Return: int
+ */
+
+void swap_int(int *a, int *b)
+{
+int tmp = *a;
+
+*a = *b;
+*b = tmp;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The String
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*(s + length) != '\0')
+length++;
+
+return (length);
+}
