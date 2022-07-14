21b49060873e49231f89a807b36a3e25fa095178
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-15 00:23:16 +0100

    advance

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,8 @@ int _strcmp(char *s1, char *s2);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
