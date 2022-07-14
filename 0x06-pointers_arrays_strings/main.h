1ff7bc4ba2208482b2ab247a9c329511d6902fa2
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-14 23:48:58 +0100

    pointers array strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,2 +1,8 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
