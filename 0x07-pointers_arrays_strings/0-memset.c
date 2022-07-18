879357abf9a5d310bfb3d63e7ff40c31b1ea1c96
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-18 17:23:11 +0100

    pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,15 +10,13 @@
 
  chat *_memset(char *s, char b, unsigned int n)
 {
-int size = n; /* only accept positive sizes */
+int i = 0;
 
-if (size > 0)
+while (n > 0)
 {
-int i;
-
-for (i = 0; i < size, i++)
 s[i] = b;
+n--;
+i++;
 }
-
 return (s);
 }
