82967a092601eb56d1479e0944c25d0d12a84734
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-21 14:25:13 +0100

    recursion fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,8 +4,8 @@
  * _pow_recursion - function that returns the value of x
  * raised to the power of y.
  *
- * @x: Number 
- * @y: Number of times to raised  
+ * @x: Number
+ * @y: Number of times to raised
  * Return: if y is lower than 0, the function should return -1
  */
 
@@ -16,5 +16,5 @@ return (-1);
 if (y == 0)
 return (1);
 else
-return (x * _pow_recursion(x, y -1));
+return (x * _pow_recursion(x, y - 1));
 }
