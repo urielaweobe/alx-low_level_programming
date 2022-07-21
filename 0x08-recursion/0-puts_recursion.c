ccd383d037230433aa2f0c681fbbfd2c884ff206
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-21 11:22:09 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ *
+ * @s: String to print
+ * Return: string printed
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(++s);
+}
+else
+{
+_putchar('\n');
+}
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,21 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _puts_recursion - a function that prints a string, followed by a new line
- *
- * @s: String to print
- * Return: string printed
- */
+#include <stdio.h>
 
-void _puts_recursion(char *s)
-{
-if (*s)
-{
-_putchar(*s);
-_puts_recursion(++s);
-}
-else
-{
-_putchar('\n');
-}
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
