1a00e82cfee9cdaa6f747c97b853ae567905b78f
Uriel Awe-Obe <mac@Uriel-Awe-Obe.local>
2022-07-21 11:39:54 +0100

    fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - function that prints a string in reverse. 
- * 
+ * _print_rev_recursion - function that prints a string in reverse.
+ *
  * @s: string
  * Return the string in reverse
  */
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,14 @@
  * _strlen_recursion - a function that returns the length of a string
  *
  * @s: String
- * Return: the length of a string 
+ * Return: the length of a string
  */
 
 int _strlen_recursion(char *s)
 {
-if(*s)
+if (*s)
 {
-return(1 + _strlen_recursion(++s));
+return (1 + _strlen_recursion(++s));
 }
 else
 {
